300582de13eaa43d9422a54b99b9b0c9a4c15faf
jvsnowflake2023 <131869972+jvsnowflake2023@users.noreply.github.com>
2023-05-03 16:30:29 +0200

    Added function

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+def get_fruity_vice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  #Create dataframe from json
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+  
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -31,11 +38,9 @@ try:
     streamlit.error('Please select a fruit to get information')
   else:
     streamlit.write('The user entered ', fruit_choice)
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  #Create dataframe from json
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display the dat
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruity_vice_data(fruit_choice)
+# display the data
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
