f6596ac5c07deb8bf70f4329606e4facf235f0d0
jvsnowflake2023 <131869972+jvsnowflake2023@users.noreply.github.com>
2023-05-04 08:46:08 +0200

    Added a button to load the fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,18 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+def get_fruity_vice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  #Create dataframe from json
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+def get_fruit_load_list:
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute
+    my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+    return my_cur.fetchall()
+  
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -24,11 +36,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-def get_fruity_vice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  #Create dataframe from json
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
+
   
 
 streamlit.header("Fruityvice Fruit Advice!")
@@ -45,13 +53,15 @@ except URLError as e:
   streamlit.error()
 
 # Connect to Snowflake
-streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
+# Add a button to load the fruit
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+if streamlit.button('Get fruit load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list
+  streamlit.dataframe(my_data_rows)
+
+
+streamlit.stop()
 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('From streamlit')")
 
 fruit_add = streamlit.text_input('What fruit would you like to add?','Jackfruit')
